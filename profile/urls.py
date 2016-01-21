ebbca5267c6c612ee3b550ac1bbae2e2c62fe44e
Traci Armstrong <traciarms@gmail.com>
2016-01-21 06:13:32 -0800

    wip finished comments, posts and uploading pictures.

--- profile/urls.py
+++ profile/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from profile.views import UpdateProfile
+from profile.views import UpdateProfile, ViewImages, ProfileList
 
 __author__ = 'traciarms'
-
 
 
 urlpatterns = [
@@ -12,4 +11,16 @@ urlpatterns = [
 
     url(r'^update_profile/(?P<profile_id>[0-9]+)/',
         login_required(UpdateProfile.as_view()), name='update_profile'),
+
+    url(r'^upload_images/',
+        'profile.views.upload_image', name='upload_image'),
+
+    url(r'^view_images/',
+        ViewImages.as_view(), name='view_images'),
+
+    url(r'^add_wall_post/', 'profile.views.add_wall_post', name='add_wall_post'),
+
+    url(r'^add_comment/(?P<wall_post_id>[0-9]+)/', 'profile.views.add_comment', name='add_comment'),
+
+    url(r'^list_profiles/', ProfileList.as_view(), name='list_profiles'),
 ]
