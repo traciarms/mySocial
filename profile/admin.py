de9f0f3210a038d6343e8a92af2ba823bc5f4f0c
Traci Armstrong <traciarms@gmail.com>
2016-01-21 06:13:32 -0800

    wip finished comments, posts and uploading pictures.

--- profile/admin.py
+++ profile/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from profile.models import Profile
+from django.contrib.auth.models import User
+from profile.models import Profile, Image, WallPost, PostComment
 
 __author__ = 'traciarms'
 
@@ -8,4 +9,19 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'dob', 'gender']
 
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['image']
+
+
+class WallPostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'posted_to', 'message', 'posted_at']
+
+
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'wall_post', 'comment', 'posted_at']
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(WallPost, WallPostAdmin)
+admin.site.register(PostComment, PostCommentAdmin)
--- profile/models.py
+++ profile/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from myfacebook.settings import MEDIA_ROOT
 
 __author__ = 'traciarms'
 
@@ -24,28 +23,28 @@ class Profile(models.Model):
                 format(self.user, self.dob, self.gender, self.phone))
 
 
-# class Image(models.Model):
-#     image = models.ImageField(upload_to=MEDIA_ROOT)
-#     profile = models.ManyToOneRel(Profile)
+class Image(models.Model):
+    image = models.ImageField(upload_to='documents/%Y/%m/%d')
+    profile = models.ForeignKey(Profile)
 
 
-# class WallPost(models.Model):
-#     author = models.ForeignKey(User)
-#     posted_to = models.ManyToOneRel(Profile)
-#     message = models.CharField(max_length=140)
-#     posted_at = models.DateTimeField()
-#
-#     def __str__(self):
-#         return ("Author: {}, Posted to: {} Message: {} Posted at: {}".
-#                 format(self.author, self.posted_to, self.message, self.posted_at))
+class WallPost(models.Model):
+    author = models.ForeignKey(User)
+    posted_to = models.ForeignKey(Profile)
+    message = models.CharField(max_length=140)
+    posted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return ("Author: {}, Posted to: {} Message: {} Posted at: {}".
+                format(self.author, self.posted_to, self.message, self.posted_at))
 
 
-# class PostComments(models.Model):
-#     author = models.ForeignKey(User)
-#     wall_post = models.ManyToOneRel(WallPost)
-#     comment = models.CharField(max_length=250)
-#     posted_at = models.DateTimeField()
-#
-#     def __str__(self):
-#         return ("Author: {}, Wall Post: {} Comment: {} Posted at: {}".
-#                 format(self.author, self.wall_post, self.comment, self.posted_at))
+class PostComment(models.Model):
+    author = models.ForeignKey(User)
+    wall_post = models.ForeignKey(WallPost)
+    comment = models.CharField(max_length=250)
+    posted_at = models.DateTimeField()
+
+    def __str__(self):
+        return ("Author: {}, Wall Post: {} Comment: {} Posted at: {}".
+                format(self.author, self.wall_post, self.comment, self.posted_at))
