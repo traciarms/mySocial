eb75b4ccc2b2d097cbac82545597042620b4a3b5
Traci Armstrong <traciarms@gmail.com>
2016-01-21 16:50:02 -0800

    finished profile pic uploading and comment on others wall & searching for others profiles

--- profile/urls.py
+++ profile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from profile.views import UpdateProfile, ViewImages, ProfileList
+from profile.views import UpdateProfile, ViewImages, ProfileList, \
+    ViewOtherProfile
 
 __author__ = 'traciarms'
 
@@ -15,12 +16,19 @@ urlpatterns = [
     url(r'^upload_images/',
         'profile.views.upload_image', name='upload_image'),
 
-    url(r'^view_images/',
-        ViewImages.as_view(), name='view_images'),
+    url(r'^view_images/', ViewImages.as_view(), name='view_images'),
 
-    url(r'^add_wall_post/', 'profile.views.add_wall_post', name='add_wall_post'),
+    url(r'^add_wall_post/(?P<profile_id>[0-9]+)',
+        'profile.views.add_wall_post', name='add_wall_post'),
 
-    url(r'^add_comment/(?P<wall_post_id>[0-9]+)/', 'profile.views.add_comment', name='add_comment'),
+    url(r'^add_comment/(?P<wall_post_id>[0-9]+)/',
+        'profile.views.add_comment', name='add_comment'),
 
     url(r'^list_profiles/', ProfileList.as_view(), name='list_profiles'),
+
+    url(r'^update_profile_image/',
+        'profile.views.update_profile_image', name='update_profile_image'),
+
+    url(r'^other_profile/(?P<profile_id>[0-9]+)/',
+        ViewOtherProfile.as_view(), name='other_profile'),
 ]
